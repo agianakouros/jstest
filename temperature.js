f0a31bfda9f0c77064a75298275538c7e38a1980
Alexander Gianakouros <agianakouros@ucvts.org>
2019-11-25 10:56:23 -0500

    curve curve curve curve

---
+++ temperature.js
@@ -0,0 +1,51 @@
+const readlineSync = require("readline-sync");
+
+const MIN = Number.MIN_SAFE_INTEGER;
+const MAX = Number.MAX_SAFE_INTEGER;
+
+
+let origin = "";
+let destination = "";
+let temp = 0;
+let temp2;
+
+console.log()
+
+while (origin !== "c" && origin !== "f" && origin !== "k") {
+  origin = (readlineSync.question("Enter an origin scale: ")).toLowerCase();
+}
+
+while (destination !== "c" && destination !== "f" && destination !== "k") {
+  destination = (readlineSync.question("Enter a destination scale: ")).toLowerCase();
+}
+
+while (temp > MAX || temp < MIN) {
+  temp = (readlineSync.question("Enter a temperature: "));
+  temp2 = (temp).toLocaleString("en", { minimumFractionDigits: 0, maximumFractionDigits: 1})
+}
+
+if (origin === "c"){
+  if (destination = "f"){
+temp = (temp * (9 / 5)) + 32
+console.log(temp)
+  } else if (destination = "k"){
+temp = temp + 273.15
+console.log(temp)
+  }
+} else if (origin === "f"){
+  if (destination = "c"){
+temp = (temp - 32) * (5 / 9)
+console.log(temp)
+  } else if (destination = "k"){
+temp = (temp - 32) * (5 / 9) + 273.15
+console.log(temp)
+} else if (origin === "k"){
+    if (destination = "c"){
+  temp = temp - 273.15
+  console.log(temp)
+} else if (destination = "f"){
+  temp = (temp - 273.15) * (5 / 9) + 32
+  console.log(temp)
+    }
+  }
+}
